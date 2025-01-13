bc09f4d5aa887101152b876edd7aa6e476d2ad77
Joe Thewlis <joethewlis@live.co.uk>
2025-01-13 05:44:57 +0000

    post model

--- allergenerosity/settings.py
+++ allergenerosity/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i2v%6sym8)p&7+)!55055ql^tx!3kl90yypx@-(618h@$6-04&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-joeht112-psychicgazpach-7u95oytyqdo.ws.codeinstitute-ide.net',]
+ALLOWED_HOSTS = ['8000-joeht112-psychicgazpach-7u95oytyqdo.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
@@ -74,12 +78,21 @@ WSGI_APPLICATION = 'allergenerosity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#}
+
+DATABASES ={
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+]
 
 
 # Password validation
